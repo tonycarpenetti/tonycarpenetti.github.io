8d4c51279520d6b6df3018772905cd73269c6336
Tony <tonycarpenetti@users.noreply.github.com>
2024-12-16 22:55:18 -0600

    Added CSS / JavaScript transitions.

--- css/portfolio.js
+++ css/portfolio.js
@@ -1,5 +1,3 @@
 window.onload = function() {
-    const element = document.getElementById('body1');
-    console.log(element);
-    element.style.top = '0';
+    document.body.classList.add('fly-in');  // Add the class to trigger the transition
 };
