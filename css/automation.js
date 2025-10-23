cc155fa0a5571c22051c62a43f3676941b079d29
tonycarpenetti <carpenetti2013@gmail.com>
2025-10-23 16:38:48 -0500

    Streamlining resume updates

---
+++ css/automation.js
@@ -0,0 +1,8 @@
+// automation.js 
+window.onload = function() {
+    document.body.classList.add('fly-in');  // Add the class to trigger the transition
+};
+
+const resumeDate = "10-23-25"; // update this once
+document.getElementById("resume-link").href = `./images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
+document.querySelector(".resume-thumbnail").src = `./images/resume-thumbnail_${resumeDate}.png`;
--- css/portfolio.js
+++
@@ -1,3 +0,0 @@
-window.onload = function() {
-    document.body.classList.add('fly-in');  // Add the class to trigger the transition
-};
