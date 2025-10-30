cb498ab12be74c2f5c90e18e44a9c25d4bb445da
tonycarpenetti <carpenetti2013@gmail.com>
2025-10-30 17:21:10 -0500

    Something is wrong with the light background and the width of the dropdown menu.

--- js/automation.js
+++ js/automation.js
@@ -1,4 +1,6 @@
 // automation.js 
+// console.log('automation.js is loaded');
+
 window.onload = function() {
     document.body.classList.add('fly-in');  // Add the class to trigger the transition
 };
@@ -6,3 +8,4 @@ window.onload = function() {
 const resumeDate = "10-23-25"; // update this once
 document.getElementById("resume-link").href = `./images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
 document.querySelector(".resume-thumbnail").src = `./images/resume-thumbnail_${resumeDate}.png`;
+
