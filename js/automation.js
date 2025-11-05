5680d19032dd732b374b813187f95cab6ece70d6
tonycarpenetti <carpenetti2013@gmail.com>
2025-11-05 07:46:10 -0600

    Trying to address background light image not showing on published site - only appears on live preview. Also did some error cleanup.

--- js/automation.js
+++ js/automation.js
@@ -5,7 +5,12 @@ window.onload = function() {
     document.body.classList.add('fly-in');  // Add the class to trigger the transition
 };
 
-const resumeDate = "10-23-25"; // update this once
-document.getElementById("resume-link").href = `./images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
-document.querySelector(".resume-thumbnail").src = `./images/resume-thumbnail_${resumeDate}.png`;
 
+window.addEventListener("DOMContentLoaded", () => {
+  const resumeDate = "10-23-25";
+  const link = document.getElementById("resume-link");
+  const thumbnail = document.querySelector(".resume-thumbnail");
+
+  if (link) link.href = `./images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
+  if (thumbnail) thumbnail.src = `./images/resume-thumbnail_${resumeDate}.jpg`;
+});
