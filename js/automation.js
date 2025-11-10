e2ef4a9b3771082ed8b05571be7fbf6653b1088a
tonycarpenetti <carpenetti2013@gmail.com>
2025-11-10 09:54:34 -0600

    Added stars to background of non-home pages.

--- js/automation.js
+++ js/automation.js
@@ -14,3 +14,18 @@ window.addEventListener("DOMContentLoaded", () => {
   if (link) link.href = `/images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
   if (thumbnail) thumbnail.src = `/images/resume-thumbnail_${resumeDate}.png`;
 });
+
+
+const starsContainer = document.querySelector('.stars-container');
+const numStars = 150 // Number of stars to generate
+
+for (let i = 0; i < numStars; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+  star.style.left = `${Math.random() * 100}%`; // Random horizontal position
+  star.style.top = `${Math.random() * 100}%`; // Random vertical position
+  star.style.width = `${Math.random() * 3 + 1}px`; // Random size
+  star.style.height = star.style.width; // Keep star circular
+  star.style.animationDelay = `${Math.random() * 2}s`; // Randomize animation start
+  starsContainer.appendChild(star);
+}
