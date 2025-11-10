fc93bbd29bcaf9d6acb3759200be6cd1564ff028
Tony <tonycarpenetti@users.noreply.github.com>
2025-11-09 23:45:24 -0600

    Major updates going from awkward positioning math to flex wraps. Very happy with the progress.

--- js/automation.js
+++ js/automation.js
@@ -9,7 +9,7 @@ window.onload = function() {
 window.addEventListener("DOMContentLoaded", () => {
   const resumeDate = "10-23-25";
   const link = document.getElementById("resume-link");
-  const thumbnail = document.getElementbyID("resume-thumbnail");
+  const thumbnail = document.getElementById("resume-thumbnail");
 
   if (link) link.href = `/images/Tony-Carpenetti-Resume_${resumeDate}.pdf`;
   if (thumbnail) thumbnail.src = `/images/resume-thumbnail_${resumeDate}.jpg`;
